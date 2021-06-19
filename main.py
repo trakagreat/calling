5180ccd1fe037799e6bdb3a53c2fc28bd1421965
Trivendra <trivendra.rahi05@gmail.com>
2021-06-19 13:37:29 +0530

    second commit

--- main.py
+++ main.py
@@ -8,17 +8,19 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 df = None
 col_index = None
 
+
 @app.route('/')
 def hello_world():
     global df
-    df=None
+    df = None
+    print('we are on welcome page')
     return render_template("index.html")
 
 
 def cleaning_df():
     df.dropna(axis=0, how='all', inplace=True)
     df.dropna(axis=1, how='all', inplace=True)
-    df.fillna("",inplace=True)
+    df.fillna("", inplace=True)
 
 
 def read_list(file):
@@ -29,6 +31,7 @@ def read_list(file):
 
 @app.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
+    print('going to choose column index')
     if request.method == 'POST':
         f = request.files['file']
         f.save(secure_filename(f.filename))
@@ -38,33 +41,34 @@ def upload_file():
 
 @app.route('/choose_col_index', methods=['GET', 'POST'])
 def choose_column_index():
+    print('we are on the chose column page ')
     columns = df.columns
     # head_rows = df.
     print(columns)
-    return render_template('choose2.html', cols=columns, df=df,pd=pd)
+    return render_template('choose2.html', cols=columns, df=df, pd=pd)
 
 
 @app.route('/choose_col', methods=['GET', 'POST'])
 def choose_col():
-    global df
+    print('we are on chose column name and contact page')
+    global df, cols
     global col_index
     if request.method == "POST":
-        col_str =request.form.get("column_index")
+        col_str = request.form.get("column_index")
         print(col_str)
         if col_str == "default":
             cols = df.columns
             df = df.reset_index()
             df.index = list(range(1, len(df) + 1))
-            df.drop("index",axis=1,inplace=True)
-            df.index.name="Serial No."
+            df.drop("index", axis=1, inplace=True)
+            df.index.name = "Serial No."
         else:
             col_index = int(col_str)
             df.columns = df.iloc[col_index]
-            df= df[col_index+1:].reset_index()
+            df = df[col_index + 1:].reset_index()
             df.index = list(range(1, len(df) + 1))
             df.drop("index", axis=1, inplace=True)
-            df.index.name="Serial No."
-
+            df.index.name = "Serial No."
 
             # df.drop(index=col_index, inplace=True)
 
@@ -78,20 +82,21 @@ def choose_col():
 
 @app.route('/uploadpage', methods=['GET', 'POST'])
 def upload_page():
+    print('we are on upload page')
     global df
-    df=None
+    df = None
     return render_template("upload.html", df=df)
 
 
 @app.route('/printlist', methods=['GET', 'POST'])
 def print_list():
     if request.method == 'POST':
-        contact_tag =df.columns[int(request.form.get("contact_tag"))]
-        name_tag =df.columns[int(request.form.get('name_tag'))]
+        contact_tag = df.columns[int(request.form.get("contact_tag"))]
+        name_tag = df.columns[int(request.form.get('name_tag'))]
         cols = [contact_tag, name_tag]
         print(cols)
 
-    return render_template("listpage.html", contact_tag=contact_tag, name_tag=name_tag, df=df,col_index=col_index)
+    return render_template("listpage.html", contact_tag=contact_tag, name_tag=name_tag, df=df, col_index=col_index)
 
 
 if __name__ == '__main__':
